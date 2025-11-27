b63548db1463ca13f979d0e9dbe4df3852ecb497
ankitdeveloper7 <kumar27ankitsingh@gmail.com>
2025-11-27 16:19:03 +0530

    changes the way url are set and also modified readme file

--- package/sdk/src/browser/index.ts
+++ package/sdk/src/browser/index.ts
@@ -2,10 +2,10 @@ import { SDKconfig } from "../core/types";
 import { config } from "../core/utils";
 import { handleWindowerror } from "./errorsListener";
 
-export function BrowserInit({ projectId, user_id, Environment }: SDKconfig) {
+export function BrowserInit({ projectId, user_id, environment }: SDKconfig) {
   ((config.projectId = projectId),
     (config.user_id = user_id),
-    (config.Environment = Environment));
+    (config.environment = environment));
 
   handleWindowerror();
 }
--- package/sdk/src/node/index.ts
+++ package/sdk/src/node/index.ts
@@ -2,10 +2,10 @@ import { SDKconfig } from "../core/types";
 import { config } from "../core/utils";
 import { UncaughtException } from "./errorListener";
 
-export function NodeInit({ projectId, user_id, Environment }: SDKconfig) {
+export function NodeInit({ projectId, user_id, environment }: SDKconfig) {
   ((config.projectId = projectId),
     (config.user_id = user_id),
-    (config.Environment = Environment));
+    (config.environment = environment));
 
   UncaughtException();
 }
